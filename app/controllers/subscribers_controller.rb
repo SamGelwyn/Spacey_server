837578c59af6f34a37d5137d983aecd65ada7a3c
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-24 15:18:55 +1000

    subscribers controller

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,18 @@
 class SubscribersController < ApplicationController
+  def create
+    subscriber = Subscriber.create!(
+      email: params['subscriber']['email'],
+      first_name: params['subscriber']['first_name'],
+      last_name: params['subscriber']['last_name']
+    )
+
+    if subscriber
+      render json: {
+        status: :created,
+        subscriber: subscriber
+      }
+    else
+      render json: { status: 500 }
+    end
+  end
 end
