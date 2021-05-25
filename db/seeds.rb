3328ffc826f8368440f126bea2c806aa02635b10
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-25 12:13:13 +1000

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+u1 = User.create :email => 'craigsy@ga.co', :password => 'chicken', :password_confirmation => 'chicken'
+u2 = User.create :email => 'jonesy@ga.co', :password => 'chicken',:password_confirmation => 'chicken'
+9.times do |n|
+  email = "example-#{n+1}@ga.co"
+  password = "chicken"
+  User.create!(
+               email: email,
+               password:              password,
+               password_confirmation: password)
+  end
+puts "#{ User.count } users"
+
+Subscriber.destroy_all
+9.times do |n|
+  first_name  = Faker::Name.first_name
+  last_name  = Faker::Name.last_name
+  email = "example-#{n+1}@ga.co"
+  Subscriber.create!(
+    first_name:  first_name,
+    last_name:  last_name,
+    email: email,
+    deals: true,
+    reviews: true,
+    packages: true,
+    star_ships: true,
+    mars: true,
+    jupiter: true,
+    moon: true,
+    saturn: true,
+    uranus: true,
+    neptune: true,
+    the_nearest_black_hole: true,
+    the_endless_void: true,
+
+  )
+  end
+puts "#{ Subscriber.count } subscribers"
