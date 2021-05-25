16ca9701c603fe686cd25d8a9938e59978052966
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-25 14:36:10 +1000

    ah

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,7 +1,7 @@
 class SubscribersController < ApplicationController
   def index
     @subscribers = Subscriber.all
-    render json: {subscribers: @subscribers}
+    # render json: {subscribers: @subscribers}
  
   end
 
@@ -14,24 +14,24 @@ class SubscribersController < ApplicationController
     subscriber = Subscriber.create!(
       email: params['subscriber']['email'],
       first_name: params['subscriber']['first_name'],
-      last_name: params['subscriber']['last_name']
-      deals: params['subscriber']['deals']
-      reviews: params['subscriber']['reviews']
-      packages: params['subscriber']['packages']
-      star_ships: params['subscriber']['star_ships']
-      mars: params['subscriber']['mars']
-      jupiter: params['subscriber']['jupiter']
-      moon: params['subscriber']['moon']
-      saturn: params['subscriber']['saturn']
-      uranus: params['subscriber']['uranus']
-      neptune: params['subscriber']['neptune']
-      the_nearest_black_hole: params['subscriber']['the_nearest_black_hole']
-      the_endless_void: params['subscriber']['the_endless_void']
-      frequency: params['subscriber']['frequency']
-      the_sun: params['subscriber']['the_sun']
-      pluto: params['subscriber']['pluto']
-      mercury: params['subscriber']['mercury']
-      venus: params['subscriber']['venus']
+      last_name: params['subscriber']['last_name'],
+      deals: params['subscriber']['deals'],
+      reviews: params['subscriber']['reviews'],
+      packages: params['subscriber']['packages'],
+      star_ships: params['subscriber']['star_ships'],
+      mars: params['subscriber']['mars'],
+      jupiter: params['subscriber']['jupiter'],
+      moon: params['subscriber']['moon'],
+      saturn: params['subscriber']['saturn'],
+      uranus: params['subscriber']['uranus'],
+      neptune: params['subscriber']['neptune'],
+      the_nearest_black_hole: params['subscriber']['the_nearest_black_hole'],
+      the_endless_void: params['subscriber']['the_endless_void'],
+      frequency: params['subscriber']['frequency'],
+      the_sun: params['subscriber']['the_sun'],
+      pluto: params['subscriber']['pluto'],
+      mercury: params['subscriber']['mercury'],
+      venus: params['subscriber']['venus'],
     )
 
     if subscriber
