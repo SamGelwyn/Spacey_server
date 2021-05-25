82668ee27f705b02c59462314b81a4cbf5569882
SamGelwyn <78458599+SamGelwyn@users.noreply.github.com>
2021-05-25 14:13:16 +1000

    create with topics

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -15,6 +15,23 @@ class SubscribersController < ApplicationController
       email: params['subscriber']['email'],
       first_name: params['subscriber']['first_name'],
       last_name: params['subscriber']['last_name']
+      deals: params['subscriber']['deals']
+      reviews: params['subscriber']['reviews']
+      packages: params['subscriber']['packages']
+      star_ships: params['subscriber']['star_ships']
+      mars: params['subscriber']['mars']
+      jupiter: params['subscriber']['jupiter']
+      moon: params['subscriber']['moon']
+      saturn: params['subscriber']['saturn']
+      uranus: params['subscriber']['uranus']
+      neptune: params['subscriber']['neptune']
+      the_nearest_black_hole: params['subscriber']['the_nearest_black_hole']
+      the_endless_void: params['subscriber']['the_endless_void']
+      frequency: params['subscriber']['frequency']
+      the_sun: params['subscriber']['the_sun']
+      pluto: params['subscriber']['pluto']
+      mercury: params['subscriber']['mercury']
+      venus: params['subscriber']['venus']
     )
 
     if subscriber
